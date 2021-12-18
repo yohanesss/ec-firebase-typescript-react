dfea5ea66cecece0eb40b83467375c57298c69a1
Yohanes Setiawan <test27c@gmail.com>
2021-12-17 21:37:12 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Layout } from "features/Layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+      <h1 className="text-3xl my-5 font-bold underline"> Hello world! </h1>
+    </Layout>
   );
 }
 
